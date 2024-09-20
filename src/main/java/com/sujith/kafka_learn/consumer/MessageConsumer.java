24254afdaf26302bed70d7e2860843018878f7a3
Sujith <sujith.s@grootan.com>
2024-09-20 22:03:24 +0530

    first commit

---
+++ src/main/java/com/sujith/kafka_learn/consumer/MessageConsumer.java
@@ -0,0 +1,14 @@
+package com.sujith.kafka_learn.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageConsumer {
+
+    @KafkaListener(topics = "my-topic", groupId = "my-group-id")
+    public void listen(String message) {
+        System.out.println("Received message: " + message);
+    }
+
+}
---
+++ src/main/java/com/sujith/kafka_learn/controller/KafkaController.java
@@ -0,0 +1,21 @@
+package com.sujith.kafka_learn.controller;
+
+import com.sujith.kafka_learn.producer.MessageProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+
+    @Autowired
+    private MessageProducer messageProducer;
+
+    @PostMapping("/send")
+    public String sendMessage(@RequestParam("message") String message) {
+        messageProducer.sendMessage("my-topic", message);
+        return "Message sent: " + message;
+    }
+
+}
